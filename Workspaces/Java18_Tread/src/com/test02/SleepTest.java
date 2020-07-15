0bd5616127e5699a7354c6d78af4b051d777b7af
zzarbttoo <qodbwls064@gmail.com>
2020-07-15 14:29:01 +0900

    Add sql

--- Workspaces/Java18_Tread/src/com/test02/SleepTest.java
+++ Workspaces/Java18_Tread/src/com/test02/SleepTest.java
@@ -5,13 +5,13 @@ public class SleepTest {
 	public static void main(String[] args) throws InterruptedException {
 		while (true) {
 			
-			//0.5초마다 하나씩 하트가 증가한다
-			//main스레드가 while을 반복할텐데
-			//Thread.sleep을 할 때마다 0.5마다 잔다
+			//0.5珥덈쭏�떎 �븯�굹�뵫 �븯�듃媛� 利앷��븳�떎
+			//main�뒪�젅�뱶媛� while�쓣 諛섎났�븷�뀗�뜲
+			//Thread.sleep�쓣 �븷 �븣留덈떎 0.5留덈떎 �옍�떎
 			
 			for (int i = 1; i <= 10; i++) {
 				Thread.sleep(500);
-				System.out.printf("%c", '♥');
+				System.out.printf("%c", '솯');
 			}
 			System.out.println();
 		}
--- Workspaces/Java19_URI/src/com/test01/MTest04.java
+++ Workspaces/Java19_URI/src/com/test01/MTest04.java
@@ -25,8 +25,6 @@ public class MTest04 {
 		}
 		
 		
-		
-		
 	}
 	
 	
