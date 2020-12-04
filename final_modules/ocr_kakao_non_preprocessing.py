20ecdd48522fe9f6d49ca908d88c1259cbac1034
zzarbttoo <qodbwls064@gmail.com>
2020-12-04 22:38:29 +0900

    Add preprocessing

--- final_modules/ocr_kakao_non_preprocessing.py
+++ final_modules/ocr_kakao_non_preprocessing.py
@@ -4,10 +4,42 @@ import cv2
 import requests
 import sys
 
-def kakao_ocr():
-    image_path = './test1.PNG'
-    appkey = 'f40be23a0183befcdd925726da9fa7d7'
-    
+LIMIT_PX = 1024
+LIMIT_BYTE = 1024*1024  # 1MB
+LIMIT_BOX = 40
+
+
+#image_path = './engTest1.PNG'
+#appkey = 'f40be23a0183befcdd925726da9fa7d7'
+
+def kakao_ocr_resize(image_path: str):
+    """
+    ocr detect/recognize api helper
+    ocr api의 제약사항이 넘어서는 이미지는 요청 이전에 전처리가 필요.
+
+    pixel 제약사항 초과: resize
+    용량 제약사항 초과  : 다른 포맷으로 압축, 이미지 분할 등의 처리 필요. (예제에서 제공하지 않음)
+
+    :param image_path: 이미지파일 경로
+    :return:
+    """
+    image = cv2.imread(image_path)
+    height, width, _ = image.shape
+
+    if LIMIT_PX < height or LIMIT_PX < width:
+        ratio = float(LIMIT_PX) / max(height, width)
+        image = cv2.resize(image, None, fx=ratio, fy=ratio)
+        height, width, _ = height, width, _ = image.shape
+
+        # api 사용전에 이미지가 resize된 경우, recognize시 resize된 결과를 사용해야함.
+        image_path = "{}_resized.jpg".format(image_path)
+        cv2.imwrite(image_path, image)
+
+        return image_path
+    return None
+
+
+def kakao_ocr(image_path: str, appkey: str):
     """
     OCR api request example
     :param image_path: 이미지파일 경로
@@ -24,22 +56,24 @@ def kakao_ocr():
 
     return requests.post(API_URL, headers=headers, files={"image": data})
 
+def preprocessing():
+    pass
 
-def main():
-    output = kakao_ocr().json()
-    #print(len(output['result']))
 
+def main(image_path, appkey):
+
+    resize_impath = kakao_ocr_resize(image_path)
+    if resize_impath is not None:
+        image_path = resize_impath
+        print("원본 대신 리사이즈된 이미지를 사용합니다.")
+
+    output = kakao_ocr(image_path, appkey).json()
     for i in output['result']:
         print(i['recognition_words'])
 
-    #print("[OCR] output:\n{}\n".format(json.dumps(output, sort_keys=True, indent=2)))
-
 
 if __name__ == "__main__":
-    main()
-
-
-'''
-한글은 잘 인식하는데 영어 + 한글이나
-아예 영어는 오히려 인식 못하는 것 같다
-'''
+    appkey = 'f40be23a0183befcdd925726da9fa7d7'
+    #값이 넘어올 수 있도록 할 것이다
+    image_path = './koeng.PNG'
+    main(image_path, appkey)
--- final_modules/ocr_kakao_preprocessing.py
+++ final_modules/ocr_kakao_preprocessing.py
@@ -3,6 +3,8 @@ import json
 import cv2
 import requests
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 LIMIT_PX = 1024
 LIMIT_BYTE = 1024*1024  # 1MB
@@ -11,6 +13,41 @@ LIMIT_BOX = 40
 
 #image_path = './engTest1.PNG'
 #appkey = 'f40be23a0183befcdd925726da9fa7d7'
+
+#이미지 대비 높이기 
+def img_enhanced(image_path:str):
+    image = cv2.imread(image_path)
+    image_enhanced = cv2.equalizeHist(image)
+    
+
+
+
+
+#이미지 선명도 높이기
+def img_filter2D(image_path:str):
+
+    image = cv2.imread(image_path)
+
+    # sharpening 최대
+    kernel = np.array([[-1.0, -1.0 , -1.0], [-1.0, 9.0, -1.0], [-1.0, -1.0, -1.0]])
+    image_sharp = cv2.filter2D(image, -1, kernel)
+
+    image_path = "{}_filter2D.PNG".format(image_path)
+    cv2.imwrite(image_path, image)
+
+    #이미지 띄워보기
+    plt.imshow(image)
+    plt.show()
+
+    return image_path
+
+
+
+
+
+#이미지 이진화
+def adaptivee_threadholding():
+    pass
 
 def kakao_ocr_resize(image_path: str):
     """
@@ -26,13 +63,14 @@ def kakao_ocr_resize(image_path: str):
     image = cv2.imread(image_path)
     height, width, _ = image.shape
 
+
     if LIMIT_PX < height or LIMIT_PX < width:
         ratio = float(LIMIT_PX) / max(height, width)
         image = cv2.resize(image, None, fx=ratio, fy=ratio)
         height, width, _ = height, width, _ = image.shape
 
         # api 사용전에 이미지가 resize된 경우, recognize시 resize된 결과를 사용해야함.
-        image_path = "{}_resized.jpg".format(image_path)
+        image_path = "{}_resized.PNG".format(image_path)
         cv2.imwrite(image_path, image)
 
         return image_path
@@ -56,16 +94,19 @@ def kakao_ocr(image_path: str, appkey: str):
 
     return requests.post(API_URL, headers=headers, files={"image": data})
 
-def preprocessing():
-    pass
 
 
 def main(image_path, appkey):
 
+    '''
+    #image_resize
     resize_impath = kakao_ocr_resize(image_path)
     if resize_impath is not None:
         image_path = resize_impath
         print("원본 대신 리사이즈된 이미지를 사용합니다.")
+    '''
+    #image_filter2D
+    filter2D_impath = img_filter2D(image_path)
 
     output = kakao_ocr(image_path, appkey).json()
     for i in output['result']:
@@ -75,5 +116,5 @@ def main(image_path, appkey):
 if __name__ == "__main__":
     appkey = 'f40be23a0183befcdd925726da9fa7d7'
     #값이 넘어올 수 있도록 할 것이다
-    image_path = './koeng.PNG'
+    image_path = './engTest1.PNG'
     main(image_path, appkey)
